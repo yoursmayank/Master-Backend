351d7d9f18b0ad6abf238be031dfe50c45dd42b7
Mayank Sharma <yoursmayank@gmail.com>
2026-04-14 23:03:23 +0530

    Expand cr7e4_POID->cr7e4_SectionNumber for section data on production orders

--- server.js
+++ server.js
@@ -1992,6 +1992,7 @@ app.get('/api/production-orders', async (req, res) => {
 
     const queryParams = {
       $select: PRODUCTION_ORDERS_SELECT,
+      $expand: "cr7e4_POID($select=_cr7e4_sectionnumber_value;$expand=cr7e4_SectionNumber($select=cr7e4_sectionnumber,cr7e4_sectionname,cr7e4_sectionsize))",
       $orderby: 'createdon desc',
       $count: 'true',
     };
