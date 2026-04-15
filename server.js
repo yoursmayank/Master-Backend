7f974ace17ecbcc152817b08273ab8e5ae3b0ee7
Mayank Sharma <yoursmayank@gmail.com>
2026-04-15 20:56:59 +0530

    fix: remove rangefrom/rangeto from production orders SELECT, add to POID expand

--- server.js
+++ server.js
@@ -2089,8 +2089,6 @@ const PRODUCTION_ORDERS_SELECT = [
   'cr7e4_dispatchdate',
   'cr7e4_productionshift',
   'cr7e4_productionpress',
-  'cr7e4_rangefrom',
-  'cr7e4_rangeto',
   'cr7e4_productionstatus',
   'cr7e4_completedkgs',
   'cr7e4_completedpcs',
@@ -2121,7 +2119,7 @@ app.get('/api/production-orders', async (req, res) => {
 
     const queryParams = {
       $select: PRODUCTION_ORDERS_SELECT,
-      $expand: "cr7e4_POID($select=_cr7e4_sectionnumber_value,cr7e4_cutlength,cr7e4_clunit;$expand=cr7e4_SectionNumber($select=cr7e4_sectionnumber,cr7e4_sectionname,cr7e4_sectionsize))",
+      $expand: "cr7e4_POID($select=_cr7e4_sectionnumber_value,cr7e4_cutlength,cr7e4_clunit,cr7e4_rangefrom,cr7e4_rangeto;$expand=cr7e4_SectionNumber($select=cr7e4_sectionnumber,cr7e4_sectionname,cr7e4_sectionsize))",
       $orderby: 'createdon desc',
       $count: 'true',
     };
